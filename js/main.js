e5657e99191e3955f50cdcf3c37de494173018b7
Liza <liza.vaivala@gmail.com>
2019-05-22 19:29:48 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".fancybox").fancybox({
+		openEffect: "none",
+		closeEffect: "none"
+	});
+	$(".zoom").hover(function(){
+		$(this).addClass('transition');
+	}, function(){
+		$(this).removeClass('transition');
+	});
+});
